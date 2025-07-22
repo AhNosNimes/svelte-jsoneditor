eccbb3d065366e0ac4e71fbfa6daaea8942f16d4
Matthis Paita <matthis.paita@emersya.com>
2025-07-22 18:29:27 +0200

    build

--- src/lib/components/controls/tooltip/Tooltip.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let text: string
-</script>
-
-<div class="jse-tooltip">
-  {text}
-</div>
-
-<style src="./Tooltip.scss"></style>
--- src/lib/components/modals/sortModalStates.ts
+++
@@ -1,8 +0,0 @@
-import type { NumberOption, PathOption } from '$lib/types.js'
-
-export interface SortModalState {
-  selectedProperty: PathOption
-  selectedDirection: NumberOption
-}
-
-export const sortModalStates: Record<string, SortModalState> = {}
